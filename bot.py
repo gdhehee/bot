258f225df58068da02329ae5b8407d233e61337b
gdhehee <ca1lender@hotmail.com>
2025-03-24 15:34:55 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 import discord
 from discord.ext import commands
 import os
+from flask import Flask
+from threading import Thread
 from dotenv import load_dotenv
 
 load_dotenv()  # Loads .env file
@@ -12,8 +14,18 @@ COMMAND_PREFIX = "!"
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 
-# Detect Render port for web service (if necessary)
-port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no port is found
+# Create Flask app for web service
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+# This function will allow the bot to run with Flask in the same process
+def run_flask():
+    # Use the Render-provided PORT, if available; fallback to 10000 if not set
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if no port is found
+    app.run(host="0.0.0.0", port=port)
 
 # Load all cogs from the cogs folder
 for filename in os.listdir("./cogs"):
@@ -25,5 +37,9 @@ async def on_ready():
     print(f"{bot.user.name} is online!")
     await bot.change_presence(activity=discord.Game(name="UltimateBot Active"))
 
-# Start the bot as usual (Web service won't be needed unless you have a web server in the bot)
+# Start Flask in a separate thread to prevent blocking
+thread = Thread(target=run_flask)
+thread.start()
+
+# Start the bot as usual
 bot.run(TOKEN)
